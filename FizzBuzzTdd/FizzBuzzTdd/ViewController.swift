4ef63e7cad5d2e571d0d1e1830564819d2046662
Igor Kruglik <>
2020-03-25 11:16:43 +0100

    initial commit

---
+++ FizzBuzzTdd/FizzBuzzTdd/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FizzBuzzTdd
+//
+//  Created by Igor Kruglik on 25.03.20.
+//  Copyright © 2020 Igor Kruglik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ FizzBuzzTdd/FizzBuzzTddTests/FizzBuzzTddTests.swift
@@ -0,0 +1,12 @@
+//
+//  FizzBuzzTddTests.swift
+//  FizzBuzzTddTests
+//
+//  Created by Igor Kruglik on 25.03.20.
+//  Copyright © 2020 Igor Kruglik. All rights reserved.
+//
+
+import XCTest
+
+@testable import FizzBuzzTdd
+
