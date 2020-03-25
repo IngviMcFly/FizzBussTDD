d969dccb44352e8e3e2aedf721cefaaf19b7d051
Igor Kruglik <>
2020-03-25 11:20:08 +0100

    make first test RED

--- FizzBuzzTdd/FizzBuzzTddTests/FizzBuzzTddTests.swift
+++ FizzBuzzTdd/FizzBuzzTddTests/FizzBuzzTddTests.swift
@@ -10,3 +10,25 @@ import XCTest
 
 @testable import FizzBuzzTdd
 
+/*
+ Write a program that prints the numbers from 1 to 100
+ For numbers divisible by 3, print “Fizz”
+ For numbers divisible by 5, print “Buzz”
+ For numbers divisible by both 3 and 5, print “FizzBuzz”
+ */
+
+class FizzBuzz {
+    
+    static func makeFizzBuzz(_ count: Int) -> String {
+        return ""
+    }
+    
+}
+
+class FizzBuzzTddTests: XCTestCase {
+
+    func test_resultForOne() {
+        XCTAssertEqual(FizzBuzz.makeFizzBuzz(1), "1")
+    }
+
+}
